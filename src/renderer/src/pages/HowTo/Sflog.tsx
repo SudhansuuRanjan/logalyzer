99c0e774506cf6cc64b2993cdc337bed58cf1137
SudhansuuRanjan <sudhanshuranjan2k18@gmail.com>
2025-08-10 14:14:45 +0530

    feat: Update Sflog page with detailed instructions for IPSG and Meat cards, GTT actions, and FTP server configuration

--- src/renderer/src/pages/HowTo/Sflog.tsx
+++ src/renderer/src/pages/HowTo/Sflog.tsx
@@ -10,29 +10,55 @@ const Sflog = () => {
 
             <Markdown>
 {`
-## SFL
+## How to run Sflog Traffic
 
-The SFL (Service Function Chaining) feature allows you to create a chain...
+ent-dstn:dpci=3-47-5
 
-### Configuration
+## Enter SFLOG IPSM Card
+ent-card:loc=1108:type=ipsm:appl=ips:sflog=yes
+chg-ip-lnk:loc=1108:port=a:ipaddr=10.75.147.105:submask=255.255.255.0:speed=100:duplex=full:mcast=no
+ent-ip-host:host=ipsm1108a:ipaddr=10.75.147.105
+chg-ip-card:loc=1108:srchordr=local:domain=new.com:defrouter=10.75.147.1
+alw-card:loc=1108
 
-To configure SFL, you need to define the service functions...
+## Enter Meat Card
+ent-card:loc=1207:type=slic:appl=ipsg
+CHG-IP-LNK:loc=1207:IPADDR=10.254.101.49:PORT=A:SUBMASK=255.255.255.0:DUPLEX=FULL:SPEED=100:auto=no
+CHG-IP-CARD:loc=1207:DEFROUTER=10.254.101.1
+ent-ip-host:ipaddr=10.254.101.49:host=ipsg1207:type=local
+ent-ip-host:ipaddr=10.254.101.197:host=meat001:type=remote
 
-### Example
+## Configure links
+ent-ls:lsn=lsmeat001:lst=a:apci=3-47-5:adapter=m3ua:maxslktps=12000:ipsg=yes:rsvdslktps=93:tpsalmtype=maxslktps
+ent-assoc:aname=meat001m3ua:lhost=ipsg1207:rhost=meat001:lport=8180:rport=5150:adapter=m3ua:bufsize=200
+ent-slk:loc=1207:lsn=lsmeat001:slc=0:port=a:aname=meat001m3ua
+ent-rte:dpci=3-47-5:lsn=lsmeat001:rc=10
+chg-assoc:open=yes:aname=meat001m3ua
+chg-ls:lsn=lsmeat001:rcontext=10
+act-slk:loc=1207:port=a
+rtrv-slk:loc=1207:port=a
+rtrv-ls:lsn=lsmeat001
+alw-card:loc=1207
 
-Here is an example:
 
-\`\`\`js
-let x = 3;
-function y() {
-  return x + 1;
-}
-\`\`\`
+## Enter GTT Action
+ent-gttact:actid=sflog1:act=sflog
+ent-gttaset:actsn=aset1:actid1=sflog1
 
-In this example, we are creating a chain...
-- base
-- headless
+ent-gttset:netdom=itu:gttsn=set1itu:settype=cdgta
+ent-gttsel:gtii=2:tt=201:cdgttsn=set1itu
+ent-gta:xlat=dpc:ri=gt:pci=3-47-5:gta=2222222220:gttsn=set1itu:actsn=aset1
 
+
+## Enter FTP Server for app=sflog
+ent-ftp-serv:path="/root/Sudhanshu/sflog":ipaddr=10.75.144.216:app=sflog:prio=1:security=on:login=root
+Password : changeme
+
+dlt-gta:gta=11111111112222222220:gttsn=set1itu
+dlt-gttsel:gtii=2:tt=35
+dlt-gttset:gttsn=set1itu
+dlt-gttaset:actsn=aset1
+dlt-gttact:actid=sflog1
 `}
 </Markdown>
 
